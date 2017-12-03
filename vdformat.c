9965ec510c1262b3a723ca18c249430e0f11698a
Kireck211 <erickdesantiago11@gmail.com>
2017-12-03 17:21:43 -0600

    Finished vdformat

--- vdformat.c
+++ vdformat.c
@@ -4,20 +4,49 @@
 #include "functions.h"
 #include "vdisk.h"
 
+#define FIRST_SECTOR 2
+
 int main(int argc,char *argv[])
 {
 	int disk = atoi(argv[1]);
+	int actual_sector = FIRST_SECTOR;
+	char eraser_buffer[SECTOR_SIZE];
 	struct SECBOOTPART sbp;
 
-
 	sprintf(sbp.nombre_particion, "part%d", disk);
-	
-	//vdwritesector(int unit, int cylinder, int surface, int sector, int nsecs, char* buffer);
 
-	vdwritesector(0, 0,0,2,1, (char*)&sbp);
+	sbp.sec_inicpart = 1;		// 1 sector 
+	sbp.sec_res = 1;		// 1 sector reservado para el sector de boot de la partición
+	sbp.sec_mapa_bits_area_nodos_i = 1;// 1 sector
+	sbp.sec_mapa_bits_bloques = 6;	// 6 sectores
+	sbp.sec_tabla_nodos_i = 3;	// 3 sectores
+	sbp.sec_log_particion = 43199;		// 43199 sectores
+	sbp.sec_x_bloque = 2;			// 2 sectores por bloque
+	sbp.heads = 8;				// 8 superficies				
+	sbp.cyls = 200;				// 200 cilindros
+	sbp.secfis = 27;				// 27 sectores por track
 
+	for(int i = 0; i < 484; i++)
+		sbp.restante[i] = 'A';
 
+	// Create eraser_buffer
+	for(int i = 0; i < SECTOR_SIZE; i++)
+		eraser_buffer[i] = 0x0;
 
+	// Write sectboot in sector
+	vdwritesector(disk, 0, 0, actual_sector, sbp.sec_inicpart, (char*)&sbp);
+
+	// Change sector to write
+	actual_sector += sbp.sec_res; // secbootpart's size is one
+
+	// Format i nodes table using buffer with zeros
+	vdwritesector(disk, 0, 0, actual_sector, sbp.sec_mapa_bits_area_nodos_i, (char*)&eraser_buffer);
+
+	// Change sector to write
+	actual_sector += sbp.sec_mapa_bits_area_nodos_i;
+
+	// Format block table using buffer with zeros
+	vdwritesector(disk, 0, 0, actual_sector, sbp.sec_mapa_bits_bloques, (char*)&eraser_buffer);
 
 	return 0;
 }
--- vdisk.h
+++ vdisk.h
@@ -1,6 +1,7 @@
 #define HEADS 8
 #define SECTORS 27
 #define CYLINDERS 200 
+#define SECTOR_SIZE 512
 
 int vdwritesector(int unit, int cylinder, int surface, int sector, int nsecs, char* buffer);
 int vdreadsector(int unit, int cylinder, int surface, int sector, int nsecs, char* buffer);
