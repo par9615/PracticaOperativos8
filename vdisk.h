83ef5548f50c16855c300df0aa4c3e928fc932b7
Kireck211 <erickdesantiago11@gmail.com>
2017-12-03 09:26:31 -0600

    Adding structs

--- vdisk.h
+++ vdisk.h
@@ -2,5 +2,36 @@
 #define SECTORS 27
 #define CYLINDERS 200 
 
-int vdwritesector(int drive, int head, int cylinder, int sector, int nsecs, char *buffer);
-int vdreadsector(int drive, int head, int cylinder, int sector, int nsecs, char *buffer);
+int vdcreat();
+int vdopen();
+int vdread();
+int vdwrite();
+int vdclose();
+int vdseek();
+int vdulink();
+
+int setinode();
+
+int searchinode();
+int removeinode();
+
+int isinodefree();
+int nextinodefree();
+
+int assigninode();
+int unassigninode();
+
+int isblockfree();
+int nextfreeblock();
+
+int assignblock();
+int unassignblock();
+
+int writeblock();
+int readblock();
+
+int vdwriteseclog(int unit, int sector, char* buffer);
+int vdreadseclog(int unit, int sector, char* buffer);
+
+int vdwritesector(int unit, int cylinder, int surface, int sector, int nsecs, char* buffer);
+int vdreadsector(int unit, int cylinder, int surface, int sector, int nsecs, char* buffer);
